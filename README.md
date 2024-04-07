ee8a859edd167bc17709739417edf621c7c09503
jiahua <littleguner@gmail.com>
2024-04-07 22:43:39 +0800

    npm run dev

---
+++ README.md
@@ -0,0 +1,9 @@
+# PowerSync Vite bundling test
+
+npm works by using optimizeDeps.include
+
+```
+npm install
+npm run dev
+```
+
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: "src/index.html",
+    },
+    emptyOutDir: true,
+  },
+  envDir: "..", // Use this dir for env vars, not 'src'.
+  optimizeDeps: {
+    // Don't optimize these packages as they contain web workers and WASM files.
+    // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
+    exclude: ["@journeyapps/wa-sqlite", "@journeyapps/powersync-sdk-web"],
+    include: [
+      "uuid",
+      "js-logger",
+      "lodash/throttle",
+      "can-ndjson-stream",
+      "event-iterator"
+    ],
+  },
+  plugins: [wasm(), topLevelAwait()],
+  worker: {
+    format: "es",
+    plugins: () => [wasm(), topLevelAwait()],
+  },
+});
